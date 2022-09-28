dd93c18b3ae1a099d406240931f33a72b0449ffa
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-09-28 11:31:32 +0200

    Navbar aangepast

--- index.php
+++ index.php
@@ -11,11 +11,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-<nav class="navbar navbar-dark bg-primary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
       Snackbar Blauw
     </a>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+    <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">Login</a>
+        </li>
+    </ul>
     
   </div>
 

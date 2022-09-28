d69f5130c2f7c5f4a839b4a69ffab332de68f24b
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-09-28 09:38:07 +0200

    Login titel toegevoegd

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ require "connection.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
+    <h1 class="display-1 m-5">Login</h1>
     <form action="" method="post" class="m-3">
         <div class="w-25">
             <label for="emailInput" class="form-label">Email address</label>

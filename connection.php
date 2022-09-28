5d1263edb52d2a69dfb92801b172976919d0f345
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-09-28 09:23:33 +0200

    connection.php gemaakt

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+// Database configuratie
+$host  = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "snackbar_blauw";
+ 
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);

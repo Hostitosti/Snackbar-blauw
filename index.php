52ab4a5db5ddfff1bd954802a4837d191f0b53d6
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-09-28 09:44:00 +0200

    submit button

--- index.php
+++ index.php
@@ -24,6 +24,9 @@ require "connection.php";
             <label for="passwordInput" class="form-label">Password</label>
             <input type="password" class="form-control" id="passwordInput" placeholder="Password" name="Password">
         </div>
+        <div class="w-25 mt-3">
+            <button type="submit" class="btn btn-primary">Confirm identity</button>
+        </div>
     </form>
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>

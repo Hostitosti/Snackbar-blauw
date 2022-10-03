fe9be7c18c0784277e250cefda70d7b790145f50
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-10-03 11:12:01 +0200

    css

--- register-user.php
+++ register-user.php
@@ -34,8 +34,8 @@
     <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
-    <h1 class="display-1 m-5">Register</h1>
+<body class="bg-primary text-white">
+    <h1 class="display-1 m-3">Register gebruiker</h1>
 
     <form action="" method="post" class="m-3">
         <div class="w-25">
@@ -73,8 +73,8 @@
         </div>
 
         <div class="w-25 mt-3">
-            <button type="submit" name="submit" class="btn btn-primary">Register</button>
-            <a href="" onclick="history.back()" style="float: right;">Back</a>
+            <button type="submit" name="submit" class="btn btn-secondary">Register</button>
+            <a href="" onclick="history.back()" style="float: right; color: white;">Back</a>
         </div>
     </form>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>

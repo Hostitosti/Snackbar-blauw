d16b5adceb7f6f31d0ae7934e7812d6dcd8eb564
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-10-03 09:38:56 +0200

    grid

--- homepage.php
+++ homepage.php
@@ -9,10 +9,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snackbar blauw</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-2">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
       Snackbar Blauw
@@ -21,18 +22,19 @@
     <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        
         <li class="nav-item">
           <a class="nav-link active" href="index.php">Login</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="logout.php">Logout</a>
+          <a class="nav-link active" href="logout.php">Logout</a>
         </li>
     </ul>
-    
+
   </div>
 
 </nav>
+<div class="grid-container">
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>

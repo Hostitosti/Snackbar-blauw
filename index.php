336543cc39221907f0637edf4ff9721a61deccd4
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-10-03 11:06:30 +0200

    style login pagina

--- index.php
+++ index.php
@@ -40,8 +40,8 @@ if(isset($_POST["submit"])){
     <title>Snackbar blauw</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
-    <h1 class="display-1 m-5">Login</h1>
+<body class="bg-primary text-white">
+    <h1 class="display-1 m-3">Login Snackbar Blauw</h1>
 
     <form action="" method="post" class="m-3">
         <div class="w-25">
@@ -53,8 +53,8 @@ if(isset($_POST["submit"])){
             <input type="password" class="form-control" id="passwordInput" placeholder="Password" name="password">
         </div>
         <div class="w-25 mt-3">
-            <button type="submit" name="submit" class="btn btn-primary">Login</button>
-            <a href="register-user.php" style="float: right;">Register +</a>
+            <button type="submit" name="submit" class="btn btn-secondary">Login</button>
+            <a href="register-user.php" style="float: right; color: white;">Register +</a>
         </div>
     </form>
     
